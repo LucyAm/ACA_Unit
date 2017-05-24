e6bf70dc4866e22d98c249b1b3e05dd93fed8f25
ahovsepy <armine.hovsepyan@gmail.com>
2017-05-24 23:28:44 +0400

    junit framework example

---
+++ aca-unit/src/test/java/aca/AppTestCase.java
@@ -0,0 +1,28 @@
+package aca;
+
+import junit.framework.TestCase;
+
+public class AppTestCase extends TestCase {
+
+	protected int value1, value2;
+	protected String value3, value4;
+
+	// assigning the values
+	protected void setUp() {
+		value1 = 3;
+		value2 = 3;
+		value3 = "s";
+		value4 = "d";
+	}
+
+	public void testAdd() {
+		double result = value1 + value2;
+		assertTrue(result == 6);
+	}
+
+	public void testConcatenate( String value3, String value4){
+		App ap = new App();
+		String resultStr = ap.concatenate(value3, value4);
+		assertTrue(resultStr.equals("sd"));
+	}
+}
