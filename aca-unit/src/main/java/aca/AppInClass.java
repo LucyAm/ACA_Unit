949d7860df8d64ddc20d14f02ac3abeeaec17315
ahovsepy <dayana.hovsepyan@gmail.com>
2017-06-01 18:41:12 +0400

    Add files via upload

---
+++ aca-unit/src/main/java/aca/AppInClass.java
@@ -0,0 +1,13 @@
+package qeaca.test_unit;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppInClass 
+{
+    public int methodToTest(String arg1, String arg2) throws NumberFormatException {
+    	
+    	return Integer.parseInt(arg1) + Integer.parseInt(arg2);
+    }
+}
